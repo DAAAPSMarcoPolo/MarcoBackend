eead4b74677c3951b9d077d27dd8f0784e5731ff
Pranay Shoroff <pshoroff@hotmail.com>
2019-01-10 16:14:35 -0500

    initial commit

---
+++ todo/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Todo
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title','description','completed')
+
+admin.site.register(Todo, TodoAdmin)
