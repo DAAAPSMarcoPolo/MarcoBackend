6dd3718d4b953529f745659d3141823a3b9df623
abablani <abablani@users.noreply.github.com>
2019-02-06 21:52:45 -0500

    Adding profile avatatar to user model

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os 
 # Create your models here.
+
+def get_image_path(instance, filename):
+    return os.path.join('photos', str(instance.id), filename)
+
+class UserAvatar(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to=get_image_path, blank=True, null=True)
 
 class Todo(models.Model):
     title = models.CharField(max_length=120)
@@ -10,3 +18,4 @@ class Todo(models.Model):
     def __str__(self):
         return self.title
 
+4
