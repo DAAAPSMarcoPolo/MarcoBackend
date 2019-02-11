f4124e36499bcebe99f329ec6149fdf47f6e42aa
seanbecker15 <sean.becker15@gmail.com>
2019-02-10 19:40:20 -0500

    email works on account creation (kinda jank, but good for now)

--- todo/api.py
+++ todo/api.py
@@ -2,9 +2,12 @@ from rest_framework import viewsets, permissions, generics
 from rest_framework.response import Response
 
 from knox.models import AuthToken
+from knox.auth import TokenAuthentication
 
 from .models import Todo
 from .serializers import TodoSerializer, CreateUserSerializer, UserSerializer, LoginUserSerializer
+
+from .utils.messages import Utils
 
 class TodoViewSet(viewsets.ModelViewSet):
   permission_classes = [permissions.IsAuthenticated, ]
@@ -26,13 +29,20 @@ class AdminRegistrationAPI(generics.GenericAPIView):
             "token": AuthToken.objects.create(user)
         })
 
-class RegistrationAPI(generics.GenericAPIView):
+class AddUserAPI(generics.GenericAPIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,permissions.IsAdminUser)
+
     serializer_class = CreateUserSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+        username = request.data['username']
+        password = request.data['password']
+        msg = "Username: " + username + "\nPassword: " + password
+        Utils.send_email(self, message=msg, subject="MarcoPolo Login Credentials", recipients=[username])
         return Response({
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)
--- todo/endpoints.py
+++ todo/endpoints.py
@@ -1,14 +1,14 @@
 from django.conf.urls import include, url
 from rest_framework import routers
 
-from .api import TodoViewSet, AdminRegistrationAPI, RegistrationAPI, LoginAPI
+from .api import TodoViewSet, AdminRegistrationAPI, AddUserAPI, LoginAPI
 
 router = routers.DefaultRouter()
 router.register('todos', TodoViewSet, 'todos')
 
 urlpatterns = [
   url("^", include(router.urls)),
-  url("^auth/register/$", RegistrationAPI.as_view()),
+  url("^auth/adduser/$", AddUserAPI.as_view()),
   
   # TODO remove in production
   url("^auth/register/admin/$", AdminRegistrationAPI.as_view()),
