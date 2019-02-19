e703f7b4e812119e400586e3058e89c299303a8e
avincent6 <vincent6@purdue.edu>
2019-02-18 20:24:33 -0500

    pulls from env file for new db password

--- backend/settings.py
+++ backend/settings.py
@@ -98,8 +98,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'MarcoPolo',
-        'USER': 'Sean',
-        'PASSWORD': '',
+        'USER': 'postgres',
+        'PASSWORD':  env('DB_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
