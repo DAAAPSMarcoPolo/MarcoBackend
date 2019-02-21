d1417ef1814d1b790f4e557c73e29d8e163c16c4
abablani <abablani@users.noreply.github.com>
2019-02-21 17:46:31 -0500

    commenting out stuff correctly

--- todo/tests.py
+++ todo/tests.py
@@ -9,14 +9,14 @@ from rest_framework.test import force_authenticate
 class TestUpdatUser(TestCase):
     def setUp(self): 
         User.objects.create_user(username="TestPerson",email="testperson@gmail.com", password= "password1")
-    def testchangeemail(self):
+    """def testchangeemail(self):
         factory = APIRequestFactory
         user = User.objects.get(username='TestPerson') 
         request = factory.put(path='/user/settings/',data={'username': 'testperson2@gmail.com'}, format='json')   
     def testchangepassword(self): 
-     /*   factory = APIRequestFactory
+        factory = APIRequestFactory
         user = User.objects.get(username='TestPerson')
         request = factory.put('/user/settings/',{'title': 'new idea'}, format='json')
-        force_authenticate(request, user=user)*/
+        force_authenticate(request, user=user)"""
     
 
