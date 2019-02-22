f1843a0a1b0354be9c21c1c4f190a972ad5fc66d
abablani <abablani@users.noreply.github.com>
2019-02-21 22:59:43 -0500

    finishing tests

--- todo/tests.py
+++ todo/tests.py
@@ -8,15 +8,29 @@ from rest_framework.test import force_authenticate
 
 class TestUpdatUser(TestCase):
     def setUp(self): 
-        User.objects.create_user(username="TestPerson",email="testperson@gmail.com", password= "password1")
-    """def testchangeemail(self):
-        factory = APIRequestFactory
-        user = User.objects.get(username='TestPerson') 
-        request = factory.put(path='/user/settings/',data={'username': 'testperson2@gmail.com'}, format='json')   
-    def testchangepassword(self): 
-        factory = APIRequestFactory
+        User.objects.create_user(username="TestPerson",email="testperson@gmail.com", password= "password")
         user = User.objects.get(username='TestPerson')
-        request = factory.put('/user/settings/',{'title': 'new idea'}, format='json')
-        force_authenticate(request, user=user)"""
-    
-
+    def testchangeemail(self):
+        """factory = APIRequestFactory
+        user = User.objects.get(username='TestPerson') 
+        request = factory.put(path='/user/settings/',data={'username': 'testperson2@gmail.com'}, format='json') """
+        newemail = "testperson2@gmail.com"
+        user = User.objects.get(username='TestPerson') 
+        user.email = newemail
+        user.save()
+        user2 = User.objects.get(username='TestPerson')
+        self.assertEqual(user2.email, newemail)
+    def testchangepassword(self): 
+        newpassword = "password1"
+        user = User.objects.get(username='TestPerson') 
+        user.password = newpassword
+        user.save()
+        user2 = User.objects.get(username='TestPerson')
+        self.assertEqual(user2.password, newpassword)
+    def testadduser(self):
+        User.objects.create_user(username="TestPerson2",email="testperson2@gmail.com", password= "password")
+        try:
+            user = User.objects.get(username='TestPerson2')
+        except User.DoesNotExist:
+            self.assertEqual("hello","heeelo")
+        
