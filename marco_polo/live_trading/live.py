d79f6820c84a0add92651d30f0f56df22fed7772
dpiotti <dpiotti@purdue.edu>
2019-04-08 17:09:22 -0400

    more live stuff

--- marco_polo/live_trading/live.py
+++ marco_polo/live_trading/live.py
@@ -8,6 +8,7 @@ import pyclbr
 import logging
 import schedule
 from datetime import datetime, timedelta
+import time
 
 from marco_polo.backtesting.market_data import DataFetcher
 
@@ -20,6 +21,7 @@ class Live:
         self.universe = universe
         self.operating_funds = float(operating_funds)
         self.price_map = None
+        self.open_price_map = None
         self.api = tradeapi.REST(key_id='PK3MIMJUUKM3UT7QCLNA',
                                  secret_key='/B6IuGjp8JmhCPWkMfILmYbS91i1c4L9p02oTV9e')
         self.logger = logging.getLogger(__name__)
@@ -49,24 +51,33 @@ class Live:
 
     def init_price_map(self):
         self.logger.info('Fetching Data...')
-        now = datetime.datetime.now()
+        now = datetime.now()
         start = now - timedelta(days=180)
-        start = datetime.strptime(start, '%Y-%m-%d')
-        end = datetime.strptime(now, '%Y-%m-%d')
 
-        price_map = DataFetcher(self.universe, self.start_date, self.end_date).daily_data()
+        start = start.strftime('%Y-%m-%d')
+        end = now.strftime('%Y-%m-%d')
+
+        price_map = DataFetcher(self.universe, start, end).daily_data()
         self.price_map = price_map
+        self.open_price_map = {}
+        for symbol in price_map:
+            self.open_price_map[symbol] = price_map[symbol]['open'].values
+
+        self.price_map = self.strategy.add_tech_ind(self.price_map)
+        print(price_map)
+
         self.logger.info('Finished Fetching data.')
 
         return True
 
-    def calc_indicators(self):
-        pass
-
     def process_new_data(self):
         for symbol in self.price_map:
-            self.api.polygon.last_quote(symbol)
+            last_quote = self.api.polygon.last_quote(symbol).__dict__['_raw']
+            ask = last_quote['askprice']
+            #bid = last_quote['bidprice']
+            self.open_price_map[symbol].append(ask)
             # Calculate new indicators
+
 
         # rank here
 
@@ -92,15 +103,21 @@ class Live:
         # Do ranking and check _buy and check_sell here
 
     def run(self):
+        self.import_strategy()
+        print(self.strategy)
         pid = os.getpid()
-        print(pid)
         # Need to store the pid in django here.
+        self.trade()
 
-        schedule.every().monday.at("9:30").do(self.trade())
-        schedule.every().tuesday.at("9:30").do(self.trade())
-        schedule.every().wednesday.at("9:30").do(self.trade())
-        schedule.every().thursday.at("9:30").do(self.trade())
-        schedule.every().friday.at("9:30").do(self.trade())
+        # schedule.every().monday.at("09:30").do(self.trade)
+        # schedule.every().tuesday.at("09:30").do(self.trade)
+        # schedule.every().wednesday.at("09:30").do(self.trade)
+        # schedule.every().thursday.at("09:30").do(self.trade)
+        # schedule.every().friday.at("09:30").do(self.trade)
+        #
+        # while True:
+        #     schedule.run_pending()
+        #     time.sleep(30)
 
 
 if __name__ == '__main__':
