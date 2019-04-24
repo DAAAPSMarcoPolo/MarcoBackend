68096cb44eb4ab86a63c3f9940bc0964ae85dfc7
seanbecker15 <sean.becker15@gmail.com>
2019-04-24 00:05:24 -0400

    prevent live instance if not approved

--- marco_polo/api/live.py
+++ marco_polo/api/live.py
@@ -26,35 +26,44 @@ class LiveAPI(generics.GenericAPIView):
             mode = data['mode']
             if mode == 'start':
                 backtest_id = data['backtest']
+                backtest = Backtest.objects.get(id=backtest_id)
+                if backtest.vote_status != 'approved':
+                    return Response({'error': 'Yikes! You tried to start a live instance on a denied/pending backtest...'},
+                                    status=status.HTTP_400_BAD_REQUEST)
                 funds = data['funds']
                 keys = AlpacaAPIKeys.objects.get(id=1)
-                backtest = Backtest.objects.get(id=backtest_id)
                 strategy = Strategy.objects.get(id=backtest.strategy.id)
                 strategy = Path(strategy.strategy_file.path).stem
                 universe = UsedUniverse.objects.get(id=backtest.universe.id)
-                universe = UsedUniverseSerializer(universe, context=self.get_serializer_context()).data['stocks']
-                new_live_instance = LiveTradeInstance.objects.create(backtest_id=backtest_id, live=False, starting_cash = funds, buying_power=funds)
+                universe = UsedUniverseSerializer(
+                    universe, context=self.get_serializer_context()).data['stocks']
+                new_live_instance = LiveTradeInstance.objects.create(
+                    backtest_id=backtest_id, live=False, starting_cash=funds, buying_power=funds)
                 live_instance_id = new_live_instance.id
                 new_live_instance.save()
-                live_instance = live.Live(live_instance_id, strategy, universe, funds, keys)
+                live_instance = live.Live(
+                    live_instance_id, strategy, universe, funds, keys)
                 strategy_islive = Strategy.objects.get(id=backtest.strategy.id)
                 strategy_islive.live = True
                 strategy_islive.save()
                 from django import db
                 db.connections.close_all()
-                p = Process(target=self.run_live, args=(live_instance,), daemon=True)
+                p = Process(target=self.run_live, args=(
+                    live_instance,), daemon=True)
                 p.start()
 
                 return Response("Starting up a live instance.", status=status.HTTP_200_OK)
 
             else:
                 live_instance_id = data['id']
-                live_instance = LiveTradeInstance.objects.get(id=live_instance_id)
+                live_instance = LiveTradeInstance.objects.get(
+                    id=live_instance_id)
                 print('here')
                 try:
                     live_instance.live = False
-                    live_instance.save() 
-                    backtest = Backtest.objects.get(id=live_instance.backtest.id)
+                    live_instance.save()
+                    backtest = Backtest.objects.get(
+                        id=live_instance.backtest.id)
                     strategy = Strategy.objects.get(id=backtest.strategy.id)
                     strategy.live = False
                     strategy.save()
@@ -75,7 +84,8 @@ class LiveAPI(generics.GenericAPIView):
                     print(live_instance.pid)
 
                     # Close out open positions in thread in case the market is closed.
-                    positions = LiveTradeInstancePosition.objects.filter(live_instance=live_instance, open=True)
+                    positions = LiveTradeInstancePosition.objects.filter(
+                        live_instance=live_instance, open=True)
                     t = threading.Thread(target=self.close_positions(),
                                          args=(positions,))
                     t.setDaemon(True)
@@ -104,7 +114,8 @@ class LiveAPI(generics.GenericAPIView):
             for p in positions:
                 symbol = p.symbol
                 qty = p.qty
-                response = api.submit_order(symbol=symbol, qty=qty, side='sell', type='market', time_in_force='gtc')
+                response = api.submit_order(
+                    symbol=symbol, qty=qty, side='sell', type='market', time_in_force='gtc')
                 now = datetime.now()
                 p.close_date = now
                 p.close_price = response.filled_avg_price
@@ -114,18 +125,21 @@ class LiveAPI(generics.GenericAPIView):
         try:
             id = self.kwargs["id"]
             live_instance = LiveTradeInstance.objects.get(id=id)
-            live_instance = LiveTradeInstanceSerializer(live_instance, context=self.get_serializer_context()).data
-            positions = LiveTradeInstancePosition.objects.filter(live_trade_instance=id).values()
-            closed_positions = LiveTradeInstancePosition.objects.filter(live_trade_instance=live_instance.id, open=False)
+            live_instance = LiveTradeInstanceSerializer(
+                live_instance, context=self.get_serializer_context()).data
+            positions = LiveTradeInstancePosition.objects.filter(
+                live_trade_instance=id).values()
+            closed_positions = LiveTradeInstancePosition.objects.filter(
+                live_trade_instance=live_instance.id, open=False)
             initvalue = 0
             finalvalue = 0
-            for pos in closed_positions: 
+            for pos in closed_positions:
                 initvalue += pos.open_price * pos.qty
                 finalvalue += pos.close_price * pos.qty
             if initvalue != 0:
-                pct_gain = (finalvalue - initvalue) / initvalue  
+                pct_gain = (finalvalue - initvalue) / initvalue
             else:
-                pct_gain = 0           
+                pct_gain = 0
             live_instance_details = {
                 'live_instance': live_instance,
                 'trades': positions,
@@ -136,21 +150,24 @@ class LiveAPI(generics.GenericAPIView):
             all_live_instances = []
             live_instances = LiveTradeInstance.objects.filter(live=True)
             for live_instance in live_instances:
-                li = LiveTradeInstanceSerializer(live_instance, context=self.get_serializer_context()).data
-                positions = LiveTradeInstancePosition.objects.filter(live_trade_instance=live_instance.id).values()
-                closed_positions = LiveTradeInstancePosition.objects.filter(live_trade_instance=live_instance.id, open=False)
+                li = LiveTradeInstanceSerializer(
+                    live_instance, context=self.get_serializer_context()).data
+                positions = LiveTradeInstancePosition.objects.filter(
+                    live_trade_instance=live_instance.id).values()
+                closed_positions = LiveTradeInstancePosition.objects.filter(
+                    live_trade_instance=live_instance.id, open=False)
                 initvalue = 0
                 finalvalue = 0
-                for pos in closed_positions: 
+                for pos in closed_positions:
                     initvalue += pos.open_price * pos.qty
                     finalvalue += pos.close_price * pos.qty
                 if initvalue != 0:
-                    pct_gain = (finalvalue - initvalue) / initvalue  
+                    pct_gain = (finalvalue - initvalue) / initvalue
                 else:
-                    pct_gain = 0   
+                    pct_gain = 0
                 live_instance_details = {
                     'live_instance': li,
-                    'trades': positions, 
+                    'trades': positions,
                     'pct_gain': pct_gain
                 }
                 all_live_instances.append(live_instance_details)
@@ -160,22 +177,21 @@ class LiveAPI(generics.GenericAPIView):
 class StrategyLiveInstanceAPI(generics.GenericAPIView):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.IsAuthenticated,)
-    
+
     def get(self, request, *args, **kwargs):
         try:
             id = self.kwargs["id"]
             alllives = []
             strategy = Strategy.objects.get(id=id)
             btset = strategy.backtest_set.all()
-            for bt in btset: 
+            for bt in btset:
                 lives = bt.livetradeinstance_set.all().values()
                 for l in lives:
                     alllives.append(l)
             strategy = Strategy.objects.filter(id=id)
-            data = {'live_instances' : alllives, 'strategy_details' : strategy.values()}
+            data = {'live_instances': alllives,
+                    'strategy_details': strategy.values()}
             print(data)
             return Response(data, status=status.HTTP_200_OK)
-        except: 
-            return Response("Could not get live instances for given strategy" ,status=status.HTTP_400_BAD_REQUEST)
-
-           
+        except:
+            return Response("Could not get live instances for given strategy", status=status.HTTP_400_BAD_REQUEST)
